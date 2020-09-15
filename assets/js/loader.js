efcd39a20fa9faed1d3574b6676aa99e4fe13a51
pandawabs <pandawa.bs@gmail.com>
2020-09-15 15:17:24 +0700

    Added multi-language feature

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,7 +1,24 @@
 var __pdata;
+var __GET = {};
+
+if(document.location.toString().indexOf('?') !== -1) {
+    var query = document.location
+                .toString()
+                // get the query string
+                .replace(/^.*?\?/, '')
+                // and remove any existing hash string (thanks, @vrijdenker)
+                .replace(/#.*$/, '')
+                .split('&');
+
+    for(var i=0, l=query.length; i<l; i++) {
+        var aux = decodeURIComponent(query[i]).split('=');
+        __GET[aux[0]] = aux[1];
+    }
+}
 
 $(document).ready( function(){
     
+    var language = (__GET['lang'] == null ? 'en':__GET['lang']); //Defaut: English
     var loadingBar = new ldBar('.loader-bar', {
             'data-type':'stroke',
             'data-fill-dir':'ltr'
@@ -20,12 +37,12 @@ $(document).ready( function(){
         });
     };
 
-    var loadPersonalData = function() {
-        return axios.get('assets/file/cv_2020.min.json', {responseType: 'json'});
-        // return axios.get('assets/file/cv_2020.json', {responseType: 'json'});
+    var loadPersonalData = function(language) {
+        return axios.get('assets/file/cv_2020_'+language+'.min.json', {responseType: 'json'});
+        // return axios.get('assets/file/cv_2020_'+language+'.json', {responseType: 'json'});
     };
     
-    Promise.all([loadPersonalData(),loadMainPage()]).then(function (responses) {
+    Promise.all([loadPersonalData(language),loadMainPage()]).then(function (responses) {
         
         loadingBar.set(100,true);
         __pdata = responses[0].data;
@@ -36,7 +53,7 @@ $(document).ready( function(){
             'animation-delay':'1.75s'
         };
         
-        let hide = setTimeout(() => {
+        setTimeout(() => {
             $('.loader-container').hide();    
         }, 2500);
 
@@ -45,4 +62,6 @@ $(document).ready( function(){
     .catch(function (error) {
         console.log(error);
     });
+
+    moment.locale(language);
 });
