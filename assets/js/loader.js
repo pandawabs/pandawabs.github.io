4e5d2900e8c34e4a0a4610b2e44fe42a82e6fab6
pandawasudewa <pandawa.sudewa@hostinger.com>
2022-04-22 22:55:51 +0700

    Update resume data; Optimize codes

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,47 +1,44 @@
 var __pdata;
 var __GET = {};
 
-if(document.location.toString().indexOf('?') !== -1) {
-    var query = document.location
-                .toString()
-                // get the query string
-                .replace(/^.*?\?/, '')
-                // and remove any existing hash string (thanks, @vrijdenker)
-                .replace(/#.*$/, '')
-                .split('&');
+if (document.location.toString().indexOf('?') !== -1) {
+    let query = document.location
+                    .toString()
+                    .replace(/^.*?\?/, '')
+                    .replace(/#.*$/, '')
+                    .split('&');
 
-    for(let i=0, l=query.length; i<l; i++) {
+    for (let i = 0, l = query.length; i < l; i++) {
         let aux = decodeURIComponent(query[i]).split('=');
         __GET[aux[0]] = aux[1];
     }
 }
 
-$(document).ready( function(){
-    
-    var lang = (__GET['lang'] == null ? 'en':__GET['lang']); //Defaut: English
-    var loadingBar = new ldBar('.loader-bar', {
+$(document).ready(function() {
+
+    let lang = (__GET['lang'] == null ? 'en' : __GET['lang']); //Defaut: English
+    let loadingBar = new ldBar('.loader-bar', {
             'data-type':'stroke',
             'data-fill-dir':'ltr'
         });
-    
-    var loadMainPage = function() {
+    let loadMainPage = function() {
         return axios.request({
             url: 'main.html',
             method: 'get',
             timeout: 60000,
             onDownloadProgress: function(e) {
-                if(e.lengthComputable){
+                if (e.lengthComputable) {
                     loadingBar.set((e.loaded/e.total) * 100, false);
                 }
             }
         });
     };
 
-    var loadPersonalData = function(language) {
-        return axios.get('assets/file/cv_2020_'+language+'.min.json', {responseType: 'json'});
-        // return axios.get('assets/file/cv_2020_'+language+'.json', {responseType: 'json'});
+    let loadPersonalData = function(language) {
+        return axios.get(`assets/file/cv_${language}.min.json`, {responseType: 'json'});
+        // return axios.get(`assets/file/cv_${language}.json`, {responseType: 'json'});
     };
-    
+
     Promise.all([loadPersonalData(lang),loadMainPage()]).then(function (responses) {
         
         loadingBar.set(100,true);
@@ -49,19 +46,17 @@ $(document).ready( function(){
         $('.main-container').html(responses[1].data);
 
         let anim = {
-            'animation-duration':'0.6s',
-            'animation-delay':'1.75s'
+            'animation-duration' : '0.6s',
+            'animation-delay' : '1.75s'
         };
-        
+
         setTimeout(() => {
             $('.loader-container').hide();    
         }, 2500);
 
         $('.loader-container').addClass('ld ld-fade-out').css(anim);
     })
-    .catch(function (error) {
-        console.log(error);
-    });
+    .catch((error) => { console.log(error) });
 
     moment.locale(lang);
 });
